837430e193e2e72a8b96ab9b4e7e033c647ecd94
zhangke895 <380749533@qq.com>
2018-05-01 14:52:13 +0800

    a

---
+++ public-number/src/pages/Page1/Page1.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import style from './Page1.css';
+
+export default class Page1 extends Component {
+    render () {
+        return (
+            <div className={style.box}>
+                this is page1~
+            </div>
+        )
+    }
+}
--- public-number/webpack.dev.config.js
+++ public-number/webpack.dev.config.js
@@ -1,7 +1,47 @@
+const merge = require('webpack-merge');
 const path = require('path');
+const webpack = require('webpack');
+const commonConfig = require('./webpack.common.config.js');
 
-module.exports = {
-    // 入口
-    entry: path.join(__dirname, 'src/index.js'),
-    
-}
+const devConfig = {
+    devtool: 'inline-source-map',
+    entry: {
+        app: [
+            'babel-polyfill',
+            'react-hot-loader/patch',
+            path.join(__dirname, 'src/index.js')
+        ]
+    },
+    output: {
+        filename: '[name].[hash].js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader?modules&localIdentName=[local]-[hash:base64:5]',
+                'postcss-loader'
+            ]
+        }]
+    },
+    devServer: {
+        port: 8080,
+        contentBase: path.join(__dirname, './dist'),
+        historyApiFallback: true,
+        host: '127.0.0.1',
+        hot: true
+        /*proxy: {
+            '/api/!*': 'http://localhost:8090/$1'
+        }*/
+    }
+};
+
+module.exports = merge({
+    customizeArray (a, b, key) {
+        if (key === 'entry.app') {
+            return b;
+        }
+        return undefined;
+    }
+})(commonConfig, devConfig);
